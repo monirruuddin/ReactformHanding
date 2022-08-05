49bf9df0697c8ce3e5aa33057659acbf37f5933c
Monir Uddin <monirkm74@gmail.com>
2022-08-06 01:39:11 +0600

    add some

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './Style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import{Container,Button,Row,Col,Form} from 'react-bootstrap';
+import{Container,Row,Col,Form} from 'react-bootstrap';
 
 function App() {
  
@@ -15,10 +15,15 @@ function App() {
     url: "",
     slug: "",
   }
+
   const [dateStore,setdateStore]= useState(initialDate);
-  const [errorStore,seterrorStore]= useState(null);
+  const [errorStore,seterrorStore]= useState({});
   const [useSubmit,setuseSubmit]= useState(false);
   const  {fullName,UserName,email,date,password,confirmPassWord,url,slug}= dateStore
+  const templist = dateStore
+  
+
+
 
   const inputHandling =(e)=>{
     const {name,value}= e.target;
@@ -26,66 +31,155 @@ function App() {
       ...dateStore,
       [name]: value,
     })
-    
+    seterrorStore({
+      ...errorStore,
+      [name]: '',
+    })
+
   }
+ 
   const handleSubmit =(e)=>{
     e.preventDefault();
-    console.log(dateStore);
-    
-    
-  }
+    const errorDateStore= {
+      fullName: "",
+      UserName: "",
+      email: "",
+      date: "",
+      password: "",
+      confirmPassWord: "",
+      url: "",
+      slug: "",
+    }
+  
+    let check= false;
+    const  {fullName,UserName,email,date,password,confirmPassWord,url,slug}= dateStore;
 
+
+    if(fullName=== ""){
+      errorDateStore.fullName = "Full Name Field in required";
+      check= true
+      
+    }
+    if(UserName=== ""){
+      errorDateStore.UserName = "User Name Field in required";
+      check= true
+      
+    }
+    if(date=== ""){
+      errorDateStore.date = "Date Field in required";
+      check= true
+      
+    }
+
+    const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(email=== "" || !regEmail.test(email)){
+      errorDateStore.email = "Email Field in required"
+      check= true;
+      
+    };
+      
+    // const regPass= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    const regPass= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+                    
+    if(password=== "" || !regPass.test(password)){
+      errorDateStore.password = "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
+      check= true
+    }
+    if(confirmPassWord === "" || confirmPassWord !== password ){
+      errorDateStore.confirmPassWord = "Confirm PassWord Field in required";
+      check= true
+    }
+    if(url === "" ){
+      errorDateStore.url = "url Field in required";
+      check= true
+      
+    }
+    if(slug === "" ){
+      errorDateStore.slug = "slug Field in required";
+      check= true
+      
+    }
+    seterrorStore(errorDateStore)
+    if(check)return
+    setuseSubmit(true)
+    console.log(dateStore);
+
+  }
 
   return (
     <Container className="app">
-      <h2>Form Handling</h2>
+      
+      {useSubmit ? <h4>✅ Yeah! Submitted Your Form <h1 className="likeEle"> 👍 </h1> </h4> : <h2>Form Handling</h2>}
         <Row>
           <Col>
-          <Form onSubmit={handleSubmit}>
+          {!useSubmit && 
+          <Form onSubmit={handleSubmit} noValidate>
             <Row>
               <Col>
                   <Form.Label>Full name</Form.Label>
                   <Form.Control type="text" name="fullName" value={fullName} onChange={inputHandling}/>
+                  <div className="alertEle">{errorStore && errorStore.fullName}</div>
+                  <br />
               </Col>
               <Col>
                   <Form.Label>User Name</Form.Label>
                   <Form.Control type="text" name="UserName" value={UserName} onChange={inputHandling}/>
+                  <div className="alertEle">{errorStore && errorStore.UserName}</div>
+                  <br />
               </Col>
             </Row>
             <Row>
               <Col>
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" name="email" value={email} onChange={inputHandling}/>
+                <div className="alertEle">  {errorStore && errorStore.email}</div>
+                  <br />
               </Col>
               <Col>
                 <Form.Label>Bithday Of Date</Form.Label>
                 <Form.Control type="date" name="date" value={date} onChange={inputHandling}/>
+               <div className="alertEle"> {errorStore && errorStore.date}</div>
+                  <br />
               </Col>
             </Row>
             <Row>
               <Col>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" name="password" value={password} onChange={inputHandling}/>
+                <div className="alertEle">{errorStore && errorStore.password}</div>
+                  <br />
               </Col>
               <Col>
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control type="password" name="confirmPassWord" value={confirmPassWord} onChange={inputHandling}/>
+             <div className="alertEle"> {errorStore && errorStore.confirmPassWord}</div>
+                <br />
               </Col>
             </Row>
             <Row>
               <Col>
                 <Form.Label>URL</Form.Label>
                 <Form.Control type="text" name="url" value={url} onChange={inputHandling}/>
+                <div className="alertEle"> {errorStore && errorStore.url}</div>
+                  <br />
               </Col>
               <Col>
                 <Form.Label>slug</Form.Label>
                 <Form.Control type="text" name="slug" value={slug} onChange={inputHandling}/>
+                <div className="alertEle">{errorStore && errorStore.slug}</div>
+                  <br />
               </Col>
             </Row>
-            <input type="submit" value="Submit" />
+            <input className="btn btn-primary mt-3 btnEle" type="submit" value="Submit" />
           </Form>
+          }  
+
           </Col>
-          <Col></Col>
+          <Col>
+          <div className="contentShow">
+          <pre>{JSON.stringify(templist, undefined, 2)}</pre>
+          </div>
+          </Col>
         </Row> 
     </Container>
   );
